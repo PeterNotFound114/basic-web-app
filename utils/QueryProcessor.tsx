d21acfdbe7cd16efb650353bc1e2bf0ab70c8774
PeterNotFound114 <sicheng2@andrew.cmu.edu>
2023-09-19 16:25:05 -0400

    square + cube

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -19,14 +19,33 @@ export default function QueryProcessor(query: string): string {
     return (parseInt(query.slice(query.indexOf("plus") - 3)) + parseInt(query.slice((query.indexOf("plus") + 5)))).toString();
   }
 
+  if (query.toLowerCase().includes("minus")) {
+    return (parseInt(query.slice(query.indexOf("minus") - 3)) - parseInt(query.slice((query.indexOf("plus") + 6)))).toString();
+  }
+
   if (query.toLowerCase().includes("largest:")) {
-    const numList = query.slice(query.indexOf("largest:") + 9).split(' ,').map(parseInt)
+    const numList = query.slice(query.indexOf("largest:") + 8).split(', ').map(parseInt)
     return Math.max(...numList).toString()
   }
 
   if (query.toLowerCase().includes("multiplied by")) {
     return (parseInt(query.slice(query.indexOf("multiplied by") - 3)) * parseInt(query.slice((query.indexOf("multiplied by") + 14)))).toString()
   }
+
+  if (query.toLowerCase().includes("square and a cube")) {
+    let numberList = []
+    let resList = []
+    numberList = query.slice(query.indexOf(":") + 1).split(', ').map(parseInt)
+    for (let i = 0; i < numberList.length; i++){
+      if (Number.isInteger(Math.sqrt(numberList[i])) && Number.isInteger(Math.cbrt(numberList[i]))){
+        resList.push(numberList[i])
+      }
+    }
+    let res = ""
+    return resList.reduce((res, n) => res.concat(n.toString(), " "), res)
+  }
+  
+
 
   
 
